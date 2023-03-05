f2f2c158f915004479503bec2ab4739b3ecaced0
codysullivan1 <cody.sullivan@my.jcu.edu.au>
2023-03-06 09:37:12 +1000

    final version works (version 2.0)

--- password.py
+++ password.py
@@ -3,9 +3,7 @@ import random
 import string
 import secrets
 
-
-def generate_password(min_length=8, max_length=20, include_uppercase=True, include_lowercase=True, include_numbers=True,
-                      include_symbols=True):
+def generate_password(min_length=8, max_length=20, include_uppercase=True, include_lowercase=True, include_numbers=True, include_symbols=True):
     """Generate a random password that meets the given criteria."""
     characters = ''
     if include_uppercase:
@@ -26,51 +24,78 @@ def generate_password(min_length=8, max_length=20, include_uppercase=True, inclu
                 and include_symbols and any(c in string.punctuation for c in password)):
             return password
 
-
-def create_user(username, password):
-    """Create a new user with the given username and hashed password."""
+def create_password_hash(password):
+    """Create a SHA-256 hash of the given password."""
     salt = secrets.token_hex(16)
     password_with_salt = password + salt
     hashed_password = hashlib.sha256(password_with_salt.encode()).hexdigest()
+    return (salt, hashed_password)
+
+def create_user(username, password):
+    """Create a new user with the given username and hashed password."""
+    salt, hashed_password = create_password_hash(password)
     with open('users.txt', 'a') as file:
         file.write(f"{username}:{salt}:{hashed_password}\n")
 
+def verify_password(username, password):
+    """Verify that the given username and password match a stored user."""
+    with open('users.txt', 'r') as file:
+        for line in file:
+            fields = line.strip().split(':')
+            if fields[0] == username:
+                salt = fields[1]
+                hashed_password = fields[2]
+                password_with_salt = password + salt
+                hashed_password_to_check = hashlib.sha256(password_with_salt.encode()).hexdigest()
+                return hashed_password_to_check == hashed_password
+    return False
 
-def verify_password(password, salt, hashed_password):
-    """Verify that the given password matches the given salt and hashed password."""
-    password_with_salt = password + salt
-    hashed_password_to_check = hashlib.sha256(password_with_salt.encode()).hexdigest()
-    return hashed_password_to_check == hashed_password
+def create_account():
+    """Create a new account with a user-specified username and password."""
+    print("Creating a new account...")
+    username = input("Enter a username: ")
+    while True:
+        password = input("Enter a password (or leave blank to generate one): ")
+        if password == '':
+            password = generate_password()
+            print(f"Generated password: {password}")
+        elif password != '':
+            if verify_password(username, password) == False:
+                create_user(username, password)
+                print(f"Account created for {username}")
+                break
+            else:
+                print("This username is already taken. Please choose a different one.")
 
+
+def login():
+    """Log in with an existing account."""
+    print("Logging in...")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if verify_password(username, password):
+        print(f"Welcome, {username}!")
+    else:
+        print("Incorrect username or password. Please try again.")
 
 def main():
     """Main function to run the program."""
-    print("Welcome to the user creation program!")
-    print("Please enter a username and password to create a new user.")
-    username = input("Username: ")
-
-    generated_password = generate_password(include_uppercase=True, include_lowercase=True, include_numbers=True,
-                                           include_symbols=True)
-    print("Generated Password:", generated_password)
-
+    print("Welcome to the user login program!")
     while True:
-        user_password = input("Password (press enter to use the generated password): ")
-        if not user_password:
-            user_password = generated_password
+        print("Choose an option:")
+        print("1. Create a new account")
+        print("2. Log in with an existing account")
+        print("3. Exit")
+        choice = input("Enter your choice (1, 2, or 3): ")
+        if choice == '1':
+            create_account()
+        elif choice == '2':
+            login()
+        elif choice == '3':
+            print("Exiting program. Goodbye!")
             break
-        if (len(user_password) < 8 or len(user_password) > 20 or
-                not any(c.isupper() for c in user_password) or
-                not any(c.islower() for c in user_password) or
-                not any(c.isdigit() for c in user_password) or
-                not any(c in string.punctuation for c in user_password)):
-            print(
-                "Password must be 8-20 characters and contain at least one uppercase letter, one lowercase letter, one number, and one symbol.")
         else:
-            break
+            print("Invalid choice. Please enter 1, 2, or 3.")
 
-    create_user(username, user_password)
-    print(f"User '{username}' has been created with the password '{user_password}'.")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
